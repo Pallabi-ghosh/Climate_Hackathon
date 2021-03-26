7bbf4aef3601479058702833bf0f210204e0c085
Enqira <moenkira@yahoo.com>
2021-03-26 17:32:23 +0100

    Added back-end part, used cuncurrently to run client and server simultaneously

---
+++ back-end/api/interviews.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const router = express.Router()
+const knex = require("../database")
+
+// 	Returns all interviews
+router.get("/", async (req, res) => {
+  try {
+    console.log("working")
+    const interviews = await knex("interviews")
+    res.json("interviews should go here")
+  } catch (error) {
+    throw error
+  }
+})
+
+// 	Adds a new interview
+// router.post("/", async (req, res) => {
+//   try {
+//     console.log("post interviews")
+//     const addinterviews = await knex("interviews").insert({
+//     })
+//     res.send("done")
+//   } catch (error) {
+//     throw error
+//   }
+// })
+
+// // Returns interview by id
+// router.get("/:id", async (req, res) => {
+//   try {
+//     if (req.params.id) {
+//       const interview = await knex("interviews").where({ id: req.params.id })
+//       res.send(interview)
+//     }
+//   } catch (error) {
+//     throw error
+//   }
+// })
+
+module.exports = router
