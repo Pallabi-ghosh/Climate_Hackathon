6ba90704b30ad2aead4e2fef46bae5de0a87ace2
TITHI1244 <tithi124410@gmail.com>
2021-03-26 13:59:35 +0100

    added separate images for more clarity

--- src/components/pages/Report.js
+++ src/components/pages/Report.js
@@ -2,6 +2,9 @@ import React from "react";
 import image1 from "../../report_images/1.png";
 import image2 from "../../report_images/2.png";
 import image3 from "../../report_images/3.png";
+import image4 from "../../report_images/4.png";
+import image5 from "../../report_images/5.png";
+import image6 from "../../report_images/6.png";
 import "../../App.css";
 
 export default function Report() {
@@ -14,9 +17,13 @@ export default function Report() {
         <img alt="img-1" className="report-images" src={image1} />
         <img alt="img-2" className="report-images" src={image2} />
       </div>
-
-      <div className="img-report">
+      <div className="img-flexbox larger">
         <img alt="img-3" className="report-images" src={image3} />
+        <img alt="img-4" className="report-images" src={image4} />
+      </div>
+      <div className="img-flexbox larger">
+        <img alt="img-5" className="report-images" src={image5} />
+        <img alt="img-6" className="report-images" src={image6} />
       </div>
     </div>
   );
